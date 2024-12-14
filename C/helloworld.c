8c17b5e53a0c8362dec8220198ea85ab192a025f
kohlerka <155389479+kohlerka@users.noreply.github.com>
2024-12-14 15:27:47 +0000

    My first C program!

---
+++ C/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Include the standard input/output library
+
+int main() {
+    printf("Hello, World!\n");  // Print "Hello, World!" followed by a newline
+    printf("Hello, Kathrin!\n");  // Print "Hello, World!" followed by a newline
+    return 0;  // Return 0 to indicate the program executed successfully
+}
