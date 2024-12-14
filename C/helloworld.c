e769aaea5e37b4beb26228c1d8dbf20c5f97100d
kohlerka <155389479+kohlerka@users.noreply.github.com>
2024-12-14 15:30:50 +0000

    Add Hello All

--- C/helloworld.c
+++ C/helloworld.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello, World!\n");  // Print "Hello, World!" followed by a newline
     printf("Hello, Kathrin!\n");  // Print "Hello, World!" followed by a newline
+    printf("Hello, All!\n");  // Print "Hello, World!" followed by a newline
     return 0;  // Return 0 to indicate the program executed successfully
 }
